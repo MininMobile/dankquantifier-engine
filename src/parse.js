d174d232edae2ac0589a08312fa85fb103a0a773
MininMobile <notminin@gmail.com>
2018-08-05 16:11:45 +0200

    add config options

--- src/parse.js
+++ src/parse.js
@@ -1,6 +1,10 @@
 const ini = require("ini");
 
-const defaultConfig = {}
+const defaultConfig = {
+	requireHead: true,
+	requireHeadUpdate: true,
+	requireHeadSize: true
+}
 
 /**
  * Parse INI to HTML
@@ -17,11 +21,11 @@ function parse(data, config = defaultConfig) {
 
 		Object.keys(skin).forEach((s) => { skin[s.toLowerCase()] = skin[s]; if (s != s.toLowerCase()) delete skin[s]; })
 
-		if (!skin.head) _error("No [head] section detected."); 
-		if (!skin.head.update) _error("No skin update specified under [head].");
-		if (!(skin.head.w || skin.head.h)) _error("No skin size specified under [head].");
+		if (!skin.head && config.requireHead) _error("No [head] section detected."); 
+		if (!skin.head.update && config.requireHeadUpdate) _error("No skin update specified under [head].");
+		if (!(skin.head.w || skin.head.h) && config.requireHeadSize) _error("No skin size specified under [head].");
 
-		_data(`<html><head>${head}</head><body>${body}</body></html>`);
+		_data(`<html><head>${head}</head><body style="width: ${skin.head.w}px; height: ${skin.head.h}px;">${body}</body></html>`);
 	});
 }
 
